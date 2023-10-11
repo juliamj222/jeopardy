b1caa3b47271e0f0bb5f0d11900f2f8ae98b3bef
julia menendez jardon <juliamenendezjardon@gmail.com>
2023-10-10 20:18:07 -0400

    it's mostly working

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,6 @@
 // jeopardy plan https://docs.google.com/document/d/1c04c2d0jmkFQ-dfVAeHXimEf9s4ej6K3ss096c9ea_k/edit
 
-
-//! it doesn't change turns. it console.logs them, but doesn't display them
 //! I was trying to make it so that you can't see the question again.
-
 
 // Do not change the import statement
 import placeholderQuestions from "./placeholder-questions.js";
@@ -62,7 +59,7 @@ let next=document.querySelector("#next");
 let colContainer=document.querySelector("#colContainer");
 
 let questionDisplay1=document.querySelector("#questionDisplay1")
-//let questionDisplay2=document.querySelector("#questionDisplay2")
+let questionDisplay2=document.querySelector("#questionDisplay2")
 
 
 let currentQuestion1 = ""
@@ -71,135 +68,169 @@ let currentAnswer1 = ""
 let currentQuestion2 = ""
 let currentAnswer2 = ""
 
-let currentPlayer = "1"
+let currentPlayer = 1;
+
+let pointAmount=0;
 
 let player1Score=0;
 let player2Score=0; 
- 
+
+let hasGuessedOrPassed = false;
+
 natureCat1.forEach((nature, index)=>{
   nature.addEventListener("click",()=>{
+    console.log(nature.innerHTML)
+    pointAmount=parseInt(nature.innerHTML)
     if (window.location.pathname === "/round-1.html") {
     console.log(natureQ1[index].question);
     console.log(natureQ1[index].answer);
     currentQuestion1=natureQ1[index].question;
     currentAnswer1=natureQ1[index].answer;
     questionDisplay1.textContent=currentQuestion1;
+    nature.classList.add("clicked");
     } 
   })
 });
 
 animalCat1.forEach((animal, index)=> {
   animal.addEventListener("click",()=>{
+    console.log(animal.innerHTML)
+    pointAmount=parseInt(animal.innerHTML)
     if (window.location.pathname === "/round-1.html") {
     console.log(animalQ1[index].question)
     console.log(animalQ1[index].answer)
     currentQuestion1=animalQ1[index].question
     currentAnswer1=animalQ1[index].answer
     questionDisplay1.textContent=currentQuestion1;
+    animal.classList.add("clicked");
     }
   })
 });
 
 computerCat1.forEach((computers, index)=> {
   computers.addEventListener("click",()=>{
+    console.log(computers.innerHTML)
+    pointAmount=parseInt(computers.innerHTML)
     if (window.location.pathname === "/round-1.html") {
     console.log(computerQ1[index].question)
     console.log(computerQ1[index].answer)
     currentQuestion1=computerQ1[index].question
     currentAnswer1=computerQ1[index].answer
     questionDisplay1.textContent=currentQuestion1;
+    computers.classList.add("clicked");
     }
   })
 })
 
 mythologyCat1.forEach((mythology, index)=> {
   mythology.addEventListener("click",()=>{
+    console.log(mythology.innerHTML)
+    pointAmount=parseInt(mythology.innerHTML)
     if (window.location.pathname === "/round-1.html") {
     console.log(mythologyQ1[index].question)
     console.log(mythologyQ1[index].answer)
     currentQuestion1=mythologyQ1[index].question
     currentAnswer1=mythologyQ1[index].answer
     questionDisplay1.textContent=currentQuestion1;
+    mythology.classList.add("clicked");
     }
   })
 })
 
 historyCat1.forEach((history, index)=> {
   history.addEventListener("click",()=>{
+    console.log(history.innerHTML)
+    pointAmount=parseInt(history.innerHTML)
     if (window.location.pathname === "/round-1.html") {
     console.log(historyQ1[index].question)
     console.log(historyQ1[index].answer)
     currentQuestion1=historyQ1[index].question
     currentAnswer1=historyQ1[index].answer
     questionDisplay1.textContent=currentQuestion1;
-    document.querySelector("#guess").classList.remove("disabled") ; 
-    //call function enable
+    history.classList.add("clicked");
     }
   })
 })
 
 generalCat1.forEach((general, index)=> {
   general.addEventListener("click",()=>{
+    console.log(general.innerHTML)
+    pointAmount=parseInt(general.innerHTML)
     if (window.location.pathname === "/round-1.html") {
     console.log(generalQ1[index].question)
     console.log(generalQ1[index].answer)
     currentQuestion1=generalQ1[index].question
     currentAnswer1=generalQ1[index].answer
     questionDisplay1.textContent=currentQuestion1;
+    general.classList.add("clicked");
     }
   })
 })
 
 natureCat2.forEach((nature, index)=>{
   nature.addEventListener("click",()=>{
+    console.log(nature.innerHTML)
+    pointAmount=parseInt(nature.innerHTML)
     if (window.location.pathname === "/round-2.html") {
     console.log(natureQ2[index].question)
     console.log(natureQ2[index].answer)
     currentQuestion2=natureQ2[index].question
     currentAnswer2=natureQ2[index].answer
     questionDisplay2.textContent=currentQuestion2;
+    nature.classList.add("clicked");
     }
   })
 });
 
 animalCat2.forEach((animal, index)=> {
   animal.addEventListener("click",()=>{
+    console.log(animal.innerHTML)
+    pointAmount=parseInt(animal.innerHTML)
     if (window.location.pathname === "/round-2.html") {
     console.log(animalQ2[index].question)
     console.log(animalQ2[index].answer)
     currentQuestion2=animalQ2[index].question
     currentAnswer2=animalQ2[index].answer
     questionDisplay2.textContent=currentQuestion2;
+    animal.classList.add("clicked");
     }
   })
 });
 
 computerCat2.forEach((computers, index)=> {
   computers.addEventListener("click",()=>{
+    console.log(computers.innerHTML)
+    pointAmount=parseInt(computers.innerHTML)
     if (window.location.pathname === "/round-2.html") {
     console.log(computerQ2[index].question)
     console.log(computerQ2[index].answer)
     currentQuestion2=computerQ2[index].question
     currentAnswer2=computerQ2[index].answer
     questionDisplay2.textContent=currentQuestion2;
+    computers.classList.add("clicked");
     }
   })
 })
 
 mythologyCat2.forEach((mythology, index)=> {
   mythology.addEventListener("click",()=>{
+    console.log(mythology.innerHTML)
+    pointAmount=parseInt(mythology.innerHTML)
     if (window.location.pathname === "/round-2.html") {
     console.log(mythologyQ2[index].question)
     console.log(mythologyQ2[index].answer)
     currentQuestion2=mythologyQ2[index].question
     currentAnswer2=mythologyQ2[index].answer
     questionDisplay2.textContent=currentQuestion2;
+    mythology.classList.add("clicked");
     }
   })
 })
 
 historyCat2.forEach((history, index)=> {
   history.addEventListener("click",()=>{
+    console.log(history.innerHTML)
+    pointAmount=parseInt(history.innerHTML)
     if (window.location.pathname === "/round-2.html") {
     console.log(historyQ2[index].question)
     console.log(historyQ2[index].answer)
@@ -207,19 +238,22 @@ historyCat2.forEach((history, index)=> {
     currentAnswer2=historyQ2[index].answer
     questionDisplay2.textContent=currentQuestion2;
     document.querySelector("#guess").classList.remove("disabled") ; 
-    //call function enable
+    history.classList.add("clicked");
     }
   })
 })
 
 generalCat2.forEach((general, index)=> {
   general.addEventListener("click",()=>{
+    console.log(general.innerHTML)
+    pointAmount=parseInt(general.innerHTML)
     if (window.location.pathname === "/round-2.html") {
     console.log(generalQ2[index].question)
     console.log(generalQ2[index].answer)
     currentQuestion2=generalQ2[index].question
     currentAnswer2=generalQ2[index].answer
     questionDisplay2.textContent=currentQuestion2;
+    general.classList.add("clicked");
     }
   })
 })
@@ -242,41 +276,38 @@ function disableNextRound() {
   document.querySelector("#next").classList.add("disabled");
 }
 
-  function correctAnswerSubmitted() {
-  console.log("function works");
+function updateScoreDisplay() {
+  document.querySelector("#player1Score").textContent = (`Player 1 - Score: ${player1Score}`);
+  document.querySelector("#player2Score").textContent = (`Player 2 - Score: ${player2Score}`);
 }
 
-/* for (let index = 0; index < natureCat.length; index++) {
-  const div = natureCat[index];
-  
-  div.addEventListener("click", (e) => {
-    currentQuestion = natureQ1[index].question;
-    currentAnswer = natureQ1[index].answer; */
+function changePlayers() {
+  currentPlayer = currentPlayer === 2 ? 1 : 2;
+  console.log(currentPlayer);
+  document.querySelector("#turnRound").textContent =(`Player ${currentPlayer}, It's your turn!`)
+}
 
-    // You can also perform other actions with the current question and answer here
-/*     console.log(`Current Question: ${currentQuestion}`);
-    console.log(`Current Answer: ${currentAnswer}`);
-  });
+function addPoints() {
+  if (currentPlayer === 1) {player1Score=player1Score+pointAmount}
+  else if (currentPlayer === 2) {player2Score=player2Score+pointAmount}
 }
- */
-/* function revealQuestionAndAnswer() {
-  console.log(currentQuestion);
-  console.log(currentAnswer);
+
+function takeAwayPoints() {
+  if (currentPlayer === 1) {player1Score=player1Score-pointAmount}
+  else if (currentPlayer === 2) {player2Score=player2Score-pointAmount}
 }
-*/
 
 questionDisplay1.addEventListener("console.log(currentQuestion1)", ()=>{
     alert("you pass")
   })
 
-/* questionDisplay2.addEventListener("console.log(currentQuestion2)", ()=>{
+questionDisplay1.addEventListener("console.log(currentQuestion1)", ()=>{
     alert("you pass")
-  }) */
-
-
+  })
 
 pass.addEventListener("click", ()=>{
-  alert("you pass")
+  alert("you pass");
+  changePlayers();
 })
 
 next.addEventListener("click", ()=>{
@@ -284,29 +315,49 @@ next.addEventListener("click", ()=>{
 })
 
 guess.addEventListener("click", ()=>{
-  let userInput=document.querySelector("#userAnswer").value
-  if (userInput.toLowerCase()===currentAnswer1.toLowerCase()) {
-    correctAnswerSubmitted();
-    div.className = "gridItem";
-    //! I was trying to make it so that you can't see the question again.
-    //colContainer.appendChild();
-  } else if (userInput.toLowerCase()!=currentAnswer1.toLowerCase()) {
-    alert("Incorrect answer");
-    currentPlayer = currentPlayer === 2 ? 1 : 2;
-    console.log(currentPlayer);
-    alert(`Player ${currentPlayer}, it's your turn!`)
+  let userInput1=document.querySelector("#userAnswer1").value
+  if (userInput1.toLowerCase()===currentAnswer1.toLowerCase()) {
+    addPoints();
+    updateScoreDisplay();
+
+    console.log(`Player1:${player1Score}, Player2:${player2Score}`); 
+  } else if (userInput1.toLowerCase()!=currentAnswer1.toLowerCase()) {
+    takeAwayPoints();
+    updateScoreDisplay();
+    console.log(`Player1:${player1Score}, Player2:${player2Score}`);
+   // alert("Incorrect answer");
+    changePlayers();
+  }
+})
+
+guess.addEventListener("click", ()=>{
+  let userInput2=document.querySelector("#userAnswer1").value
+  if (userInput2.toLowerCase()===currentAnswer2.toLowerCase()) {
+    addPoints();
+    updateScoreDisplay();
+
+    console.log(`Player1:${player1Score}, Player2:${player2Score}`); 
+  } else if (userInput2.toLowerCase()!=currentAnswer2.toLowerCase()) {
+    takeAwayPoints();
+    updateScoreDisplay();
+    console.log(`Player1:${player1Score}, Player2:${player2Score}`);
+   // alert("Incorrect answer");
+    changePlayers();
   }
 })
 
 window.addEventListener("load", () => {
   if (window.location.pathname === "/round-1.html") {
-    document.querySelector("#turnRound1").textContent = `Player ${currentPlayer}'s turn!`; // turns
+    document.querySelector("#turnRound").textContent = `Player ${currentPlayer}'s turn!`; // turns
+    updateScoreDisplay();
     enable();
   } else if (window.location.pathname === "/round-2.html") {
-    document.querySelector("#turnRound2").textContent = `Player ${currentPlayer}'s turn!`; // turns
+    document.querySelector("#turnRound").textContent = `Player ${currentPlayer}'s turn!`; // turns
+    updateScoreDisplay();
     enable();
   } else if (window.location.pathname === "/final-jeopardy.html") {
-    document.querySelector("#turnRound3").textContent = `Player ${currentPlayer}'s turn!`; // turns
+    document.querySelector("#turnRound").textContent = `Player ${currentPlayer}'s turn!`; // turns
+    updateScoreDisplay();
     enable();
   }
 });
