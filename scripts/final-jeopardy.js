9dbfdee05b63d019bba24c6da7a136707a7aba87
julia menendez jardon <juliamenendezjardon@gmail.com>
2023-11-02 16:55:37 -0400

    everything works great!

--- scripts/final-jeopardy.js
+++ scripts/final-jeopardy.js
@@ -10,75 +10,121 @@ let player1Score = parseInt(searchParams.get("player1Score"));
 let player2Score = parseInt(searchParams.get("player2Score"));
 
 // Final question
-const finalQ= placeholderQuestions.find((placeholderQuestions)=> placeholderQuestions.category==="Final");
+const finalQ = placeholderQuestions.find(
+  (placeholderQuestions) => placeholderQuestions.category === "Final"
+);
+
+const finalBetAmount = document.getElementById("finalBetAmount");
+finalBetAmount.addEventListener("input", function () {
+  //take the value from the input?
+  pointAmount = parseInt(finalBetAmount.value);
+  console.log(pointAmount);
+});
 
 // Fishing
 
 // Variable that displays the questions and states the questions that will be displayed
-let questionDisplay1=document.querySelector(".questionDisplay1")
-let currentQuestion = ""
-let currentAnswer = ""
+let questionDisplay1 = document.querySelector(".questionDisplay1");
+let currentQuestion = "";
+let currentAnswer = "";
 
 // Variables that keep track of whose turn it is and their points
-let pointAmount=0;
-
-finalCategory()
-
-
-function finalCategory() {
-    if (window.location.pathname === "/final-jeopardy.html") {
-      console.log(finalQ.question);
-      console.log(finalQ.answer);
-      currentQuestion = finalQ.question;
-      currentAnswer = finalQ.answer;
-      questionDisplay1.textContent = currentQuestion;
-      
-      const finalBetInput = document.getElementById("finalBet");
-      finalBetInput.addEventListener("input", function() {
-        //take the value from the input?
-        pointAmount = parseInt(finalBetInput.value); 
-        console.log(pointAmount);
-      });
-    }
-  }
-
-let finalBet=document.querySelector("#finalBet");
+let pointAmount = 0;
+let passGuessCount = 0;
 
 // function to show the score
 function updateScoreDisplay() {
-  document.querySelector("#player1Score").textContent = (`Player 1 - Score: ${player1Score}`);
-  document.querySelector("#player2Score").textContent = (`Player 2 - Score: ${player2Score}`);
+  document.querySelector(
+    "#player1Score"
+  ).textContent = `Player 1 - Score: ${player1Score}`;
+  document.querySelector(
+    "#player2Score"
+  ).textContent = `Player 2 - Score: ${player2Score}`;
 }
+
+updateScoreDisplay();
+
+// function to change players
+function changePlayers() {
+  currentPlayer = currentPlayer === 2 ? 1 : 2;
+  console.log(currentPlayer);
+  document.querySelector(
+    "#turnRound"
+  ).textContent = `Player ${currentPlayer}, It's your turn!`;
+}
+
+function finalCategory() {
+  if (window.location.pathname === "/final-jeopardy.html") {
+    console.log(finalQ.question);
+    console.log(finalQ.answer);
+    currentQuestion = finalQ.question;
+    currentAnswer = finalQ.answer;
+    questionDisplay1.textContent = currentQuestion;
+  }
+}
+
+/* we see the finalBetAmount, check if it's value is within the player's means  */
+finalBet.addEventListener("click", () => {
+  let finalBetAmount = document.getElementById("finalBetAmount");
+  let pointAmount = finalBetAmount;
+  if (currentPlayer === 1 && finalBetAmount.value <= player1Score) {
+    console.log(finalBetAmount.value);
+    finalCategory();
+  } else if (currentPlayer === 2 && finalBetAmount.value <= player2Score) {
+    console.log(finalBetAmount.value);
+    finalCategory();
+  }
+});
 
 // function to add points
 function addPoints() {
-  if (currentPlayer === 1) {player1Score=player1Score+pointAmount}
-  else if (currentPlayer === 2) {player2Score=player2Score+pointAmount}
+  if (currentPlayer === 1) {
+    player1Score = player1Score + pointAmount;
+  } else if (currentPlayer === 2) {
+    player2Score = player2Score + pointAmount;
+  }
 }
 // function to take away points
 function takeAwayPoints() {
-  if (currentPlayer === 1) {player1Score=player1Score-pointAmount}
-  else if (currentPlayer === 2) {player2Score=player2Score-pointAmount}
+  if (currentPlayer === 1) {
+    player1Score = player1Score - pointAmount;
+  } else if (currentPlayer === 2) {
+    player2Score = player2Score - pointAmount;
+  }
 }
 
-guess.addEventListener("click", ()=>{
-  let userInput1=document.querySelector(".userAnswer1").value
-  if (userInput1.toLowerCase()===currentAnswer.toLowerCase()) {
-    addPoints(); // if right, add points
-    updateScoreDisplay(); // display score
-    console.log(`Player1:${player1Score}, Player2:${player2Score}`); 
-    alert("Congratulations! Your answer was right! You won the game! END OF THE GAME");
-  } else if (userInput1.toLowerCase()!=currentAnswer.toLowerCase()) {
-    takeAwayPoints(); // if wrong, take away points
-    updateScoreDisplay(); // display score
+guess.addEventListener("click", () => {
+  let userInput1 = document.querySelector(".userAnswer1").value;
+  if (userInput1.toLowerCase() === currentAnswer.toLowerCase()) {
+    addPoints();
+    updateScoreDisplay();
+    console.log(`Player1:${player1Score}, Player2:${player2Score}`);
+    alert(
+      "Congratulations! Your answer was right! You won the game! END OF THE GAME"
+    );
+  } else if (
+    userInput1.toLowerCase() != currentAnswer.toLowerCase() &&
+    passGuessCount === 0
+  ) {
+    takeAwayPoints();
+    passGuessCount++;
+    updateScoreDisplay();
+    console.log(`Player1:${player1Score}, Player2:${player2Score}`);
+    changePlayers();
+  } else if (userInput1.toLowerCase() != currentAnswer.toLowerCase()) {
+    takeAwayPoints();
+    updateScoreDisplay();
+    passGuessCount = 1;
     console.log(`Player1:${player1Score}, Player2:${player2Score}`);
     alert("Wrong answer! END OF THE GAME");
   }
-})
+});
 
 window.addEventListener("load", () => {
   if (window.location.pathname === "/final-jeopardy.html") {
-    document.querySelector("#turnRound").textContent = `Player ${currentPlayer}'s turn!`; // turns
+    document.querySelector(
+      "#turnRound"
+    ).textContent = `Player ${currentPlayer}'s turn!`; // turns
     updateScoreDisplay(); //updating the score after the turn
   }
 });
--- scripts/round-2.js
+++ scripts/round-2.js
@@ -9,158 +9,183 @@ let currentPlayer = parseInt(searchParams.get("currentPlayer"));
 let player1Score = parseInt(searchParams.get("player1Score"));
 let player2Score = parseInt(searchParams.get("player2Score"));
 
+//variable to keep track of clicked questions
+let clickedQuestions = 0;
+
 // Questions imported in chunks of 5
-const natureQ = placeholderQuestions.filter((placeholderQuestions) => placeholderQuestions.category === "Nature");
+const natureQ = placeholderQuestions.filter(
+  (placeholderQuestions) => placeholderQuestions.category === "Nature"
+);
 const natureQ2 = natureQ.slice(5, 10);
 
-const animalQ= placeholderQuestions.filter((placeholderQuestions)=> placeholderQuestions.category==="Animals");
+const animalQ = placeholderQuestions.filter(
+  (placeholderQuestions) => placeholderQuestions.category === "Animals"
+);
 const animalQ2 = animalQ.slice(5, 10);
 
-const computerQ= placeholderQuestions.filter((placeholderQuestions)=> placeholderQuestions.category==="Computers");
+const computerQ = placeholderQuestions.filter(
+  (placeholderQuestions) => placeholderQuestions.category === "Computers"
+);
 const computerQ2 = computerQ.slice(5, 10);
 
-const mythologyQ= placeholderQuestions.filter((placeholderQuestions)=> placeholderQuestions.category==="Mythology");
+const mythologyQ = placeholderQuestions.filter(
+  (placeholderQuestions) => placeholderQuestions.category === "Mythology"
+);
 const mythologyQ2 = mythologyQ.slice(5, 10);
 
-const historyQ= placeholderQuestions.filter((placeholderQuestions)=> placeholderQuestions.category==="History");
+const historyQ = placeholderQuestions.filter(
+  (placeholderQuestions) => placeholderQuestions.category === "History"
+);
 const historyQ2 = historyQ.slice(5, 10);
 
-const generalQ= placeholderQuestions.filter((placeholderQuestions)=> placeholderQuestions.category==="General");
+const generalQ = placeholderQuestions.filter(
+  (placeholderQuestions) => placeholderQuestions.category === "General"
+);
 const generalQ2 = generalQ.slice(5, 10);
 
 // Creating variables for every category of questions, for every round
 
-const natureCat2=document.querySelectorAll(".nature") 
-const animalCat2=document.querySelectorAll(".animals") 
-const computerCat2=document.querySelectorAll(".computers");
-const mythologyCat2=document.querySelectorAll(".mythology");
-const historyCat2=document.querySelectorAll(".history");
-const generalCat2=document.querySelectorAll(".general");
+const natureCat2 = document.querySelectorAll(".nature");
+const animalCat2 = document.querySelectorAll(".animals");
+const computerCat2 = document.querySelectorAll(".computers");
+const mythologyCat2 = document.querySelectorAll(".mythology");
+const historyCat2 = document.querySelectorAll(".history");
+const generalCat2 = document.querySelectorAll(".general");
 
 // Fishing
 // Variables for guessing, passing and next
-let guess=document.querySelector("#guess");
-let pass=document.querySelector("#pass");
-let next=document.querySelector("#next");
+let guess = document.querySelector("#guess");
+let pass = document.querySelector("#pass");
+let next = document.querySelector("#next");
 
 // Variable that displays the questions and states the questions that will be displayed
-let questionDisplay1=document.querySelector(".questionDisplay1")
-let currentQuestion = ""
-let currentAnswer = ""
+let questionDisplay1 = document.querySelector(".questionDisplay1");
+let currentQuestion = "";
+let currentAnswer = "";
 
-let pointAmount=0;
-
-
+let pointAmount = 0;
 
 // Loops so that questions from all categories are asked, selecting the rounds when they will be asked
-natureCat2.forEach((nature, index)=>{
-  nature.addEventListener("click",()=>{
-    console.log(nature.innerHTML)
-    pointAmount=parseInt(nature.innerHTML)
+natureCat2.forEach((nature, index) => {
+  nature.addEventListener("click", () => {
+    console.log(nature.innerHTML);
+    clickedQuestions++;
+    pointAmount = parseInt(nature.innerHTML);
     if (window.location.pathname === "/round-2.html") {
-    console.log(natureQ2[index].question)
-    console.log(natureQ2[index].answer)
-    currentQuestion=natureQ2[index].question
-    currentAnswer=natureQ2[index].answer
-    questionDisplay1.textContent=currentQuestion;
-    nature.classList.add("clicked");
-    disableOtherQuestions();
-    enablePassGuess();
-    disableNextRound()
+      console.log(natureQ2[index].question);
+      console.log(natureQ2[index].answer);
+      currentQuestion = natureQ2[index].question;
+      currentAnswer = natureQ2[index].answer;
+      questionDisplay1.textContent = currentQuestion;
+      nature.classList.add("clicked");
+      disableOtherQuestions();
+      enablePassGuess();
+      disableNextRound();
+      console.log(clickedQuestions);
     }
-  })
+  });
 });
 
-animalCat2.forEach((animal, index)=> {
-  animal.addEventListener("click",()=>{
-    console.log(animal.innerHTML)
-    pointAmount=parseInt(animal.innerHTML)
+animalCat2.forEach((animal, index) => {
+  animal.addEventListener("click", () => {
+    console.log(animal.innerHTML);
+    clickedQuestions++;
+    pointAmount = parseInt(animal.innerHTML);
     if (window.location.pathname === "/round-2.html") {
-    console.log(animalQ2[index].question)
-    console.log(animalQ2[index].answer)
-    currentQuestion=animalQ2[index].question
-    currentAnswer=animalQ2[index].answer
-    questionDisplay1.textContent=currentQuestion;
-    animal.classList.add("clicked");
-    disableOtherQuestions();
-    enablePassGuess();
-    disableNextRound()
+      console.log(animalQ2[index].question);
+      console.log(animalQ2[index].answer);
+      currentQuestion = animalQ2[index].question;
+      currentAnswer = animalQ2[index].answer;
+      questionDisplay1.textContent = currentQuestion;
+      animal.classList.add("clicked");
+      disableOtherQuestions();
+      enablePassGuess();
+      disableNextRound();
+      console.log(clickedQuestions);
     }
-  })
+  });
 });
 
-computerCat2.forEach((computers, index)=> {
-  computers.addEventListener("click",()=>{
-    console.log(computers.innerHTML)
-    pointAmount=parseInt(computers.innerHTML)
+computerCat2.forEach((computers, index) => {
+  computers.addEventListener("click", () => {
+    console.log(computers.innerHTML);
+    clickedQuestions++;
+    pointAmount = parseInt(computers.innerHTML);
     if (window.location.pathname === "/round-2.html") {
-    console.log(computerQ2[index].question)
-    console.log(computerQ2[index].answer)
-    currentQuestion=computerQ2[index].question
-    currentAnswer=computerQ2[index].answer
-    questionDisplay1.textContent=currentQuestion;
-    computers.classList.add("clicked");
-    disableOtherQuestions();
-    enablePassGuess();
-    disableNextRound()
+      console.log(computerQ2[index].question);
+      console.log(computerQ2[index].answer);
+      currentQuestion = computerQ2[index].question;
+      currentAnswer = computerQ2[index].answer;
+      questionDisplay1.textContent = currentQuestion;
+      computers.classList.add("clicked");
+      disableOtherQuestions();
+      enablePassGuess();
+      disableNextRound();
+      console.log(clickedQuestions);
     }
-  })
-})
+  });
+});
 
-mythologyCat2.forEach((mythology, index)=> {
-  mythology.addEventListener("click",()=>{
-    console.log(mythology.innerHTML)
-    pointAmount=parseInt(mythology.innerHTML)
+mythologyCat2.forEach((mythology, index) => {
+  mythology.addEventListener("click", () => {
+    console.log(mythology.innerHTML);
+    clickedQuestions++;
+    pointAmount = parseInt(mythology.innerHTML);
     if (window.location.pathname === "/round-2.html") {
-    console.log(mythologyQ2[index].question)
-    console.log(mythologyQ2[index].answer)
-    currentQuestion=mythologyQ2[index].question
-    currentAnswer=mythologyQ2[index].answer
-    questionDisplay1.textContent=currentQuestion;
-    mythology.classList.add("clicked");
-    disableOtherQuestions();
-    enablePassGuess();
-    disableNextRound()
+      console.log(mythologyQ2[index].question);
+      console.log(mythologyQ2[index].answer);
+      currentQuestion = mythologyQ2[index].question;
+      currentAnswer = mythologyQ2[index].answer;
+      questionDisplay1.textContent = currentQuestion;
+      mythology.classList.add("clicked");
+      disableOtherQuestions();
+      enablePassGuess();
+      disableNextRound();
+      console.log(clickedQuestions);
     }
-  })
-})
+  });
+});
 
-historyCat2.forEach((history, index)=> {
-  history.addEventListener("click",()=>{
-    console.log(history.innerHTML)
-    pointAmount=parseInt(history.innerHTML)
+historyCat2.forEach((history, index) => {
+  history.addEventListener("click", () => {
+    console.log(history.innerHTML);
+    clickedQuestions++;
+    pointAmount = parseInt(history.innerHTML);
     if (window.location.pathname === "/round-2.html") {
-    console.log(historyQ2[index].question)
-    console.log(historyQ2[index].answer)
-    currentQuestion=historyQ2[index].question
-    currentAnswer=historyQ2[index].answer
-    questionDisplay1.textContent=currentQuestion;
-    document.querySelector("#guess").classList.remove("disabled") ; 
-    history.classList.add("clicked");
-    disableOtherQuestions();
-    enablePassGuess();
-    disableNextRound()
+      console.log(historyQ2[index].question);
+      console.log(historyQ2[index].answer);
+      currentQuestion = historyQ2[index].question;
+      currentAnswer = historyQ2[index].answer;
+      questionDisplay1.textContent = currentQuestion;
+      document.querySelector("#guess").classList.remove("disabled");
+      history.classList.add("clicked");
+      disableOtherQuestions();
+      enablePassGuess();
+      disableNextRound();
+      console.log(clickedQuestions);
     }
-  })
-})
+  });
+});
 
-generalCat2.forEach((general, index)=> {
-  general.addEventListener("click",()=>{
-    console.log(general.innerHTML)
-    pointAmount=parseInt(general.innerHTML)
+generalCat2.forEach((general, index) => {
+  general.addEventListener("click", () => {
+    console.log(general.innerHTML);
+    clickedQuestions++;
+    pointAmount = parseInt(general.innerHTML);
     if (window.location.pathname === "/round-2.html") {
-    console.log(generalQ2[index].question)
-    console.log(generalQ2[index].answer)
-    currentQuestion=generalQ2[index].question
-    currentAnswer=generalQ2[index].answer
-    questionDisplay1.textContent=currentQuestion;
-    general.classList.add("clicked");
-    disableOtherQuestions();
-    enablePassGuess();
-    disableNextRound()
+      console.log(generalQ2[index].question);
+      console.log(generalQ2[index].answer);
+      currentQuestion = generalQ2[index].question;
+      currentAnswer = generalQ2[index].answer;
+      questionDisplay1.textContent = currentQuestion;
+      general.classList.add("clicked");
+      disableOtherQuestions();
+      enablePassGuess();
+      disableNextRound();
+      console.log(clickedQuestions);
     }
-  })
-})
+  });
+});
 
 // Functions to enable or disable buttons
 function enablePassGuess() {
@@ -179,122 +204,170 @@ function enableNextRound() {
 
 function disableNextRound() {
   document.querySelector("#next").classList.add("disabled");
-  };
+}
 
 function disableOtherQuestions() {
   const gridItems = document.querySelectorAll(".gridItem");
-  gridItems.forEach(function(gridItem) {
+  gridItems.forEach(function (gridItem) {
     gridItem.classList.add("disabled");
   });
 }
 
 function enableOtherQuestions() {
   const gridItems = document.querySelectorAll(".gridItem");
-  gridItems.forEach(function(gridItem) {
+  gridItems.forEach(function (gridItem) {
     gridItem.classList.remove("disabled");
   });
 }
 
 // function to show the score
 function updateScoreDisplay() {
-  document.querySelector("#player1Score").textContent = (`Player 1 - Score: ${player1Score}`);
-  document.querySelector("#player2Score").textContent = (`Player 2 - Score: ${player2Score}`);
+  document.querySelector(
+    "#player1Score"
+  ).textContent = `Player 1 - Score: ${player1Score}`;
+  document.querySelector(
+    "#player2Score"
+  ).textContent = `Player 2 - Score: ${player2Score}`;
 }
 // function to change players
 function changePlayers() {
   currentPlayer = currentPlayer === 2 ? 1 : 2;
   console.log(currentPlayer);
-  document.querySelector("#turnRound").textContent =(`Player ${currentPlayer}, It's your turn!`)
+  document.querySelector(
+    "#turnRound"
+  ).textContent = `Player ${currentPlayer}, It's your turn!`;
 }
 // function to add points
 function addPoints() {
-  if (currentPlayer === 1) {player1Score=player1Score+pointAmount}
-  else if (currentPlayer === 2) {player2Score=player2Score+pointAmount}
+  if (currentPlayer === 1) {
+    player1Score = player1Score + pointAmount;
+  } else if (currentPlayer === 2) {
+    player2Score = player2Score + pointAmount;
+  }
 }
 // function to take away points
 function takeAwayPoints() {
-  if (currentPlayer === 1) {player1Score=player1Score-pointAmount}
-  else if (currentPlayer === 2) {player2Score=player2Score-pointAmount}
+  if (currentPlayer === 1) {
+    player1Score = player1Score - pointAmount;
+  } else if (currentPlayer === 2) {
+    player2Score = player2Score - pointAmount;
+  }
 }
 
 // Function to pass scores and currentPlayer to the next round
 function goToNextRound() {
-  const url = 'http://127.0.0.1:5500/final-jeopardy.html';
+  const url = "http://127.0.0.1:5500/final-jeopardy.html";
   const searchParams = new URLSearchParams();
-  searchParams.append('player1Score', player1Score);
-  searchParams.append('player2Score', player2Score);
-  searchParams.append('currentPlayer', currentPlayer);
+  searchParams.append("player1Score", player1Score);
+  searchParams.append("player2Score", player2Score);
+  searchParams.append("currentPlayer", currentPlayer);
   const queryString = searchParams.toString();
   const URL = `${url}?${queryString}`;
+  console.log("Generated URL:", URL);
   window.location.href = URL;
 }
 
-let passGuessCount=0;
+let passGuessCount = 0;
 
 document.getElementById("pass").addEventListener("click", function () {
-  passGuessCount++;
-  if (passGuessCount ===1) {
+  if (passGuessCount === 0) {
+    passGuessCount++;
     changePlayers();
-  } else if 
-    (passGuessCount ===2) {
-      changePlayers();
-      passGuessCount =0;
-      enableOtherQuestions();
-      questionDisplay1.textContent=" ";
-      disablePassGuess();
+    enableNextIfAllQuestionsClicked();
+  } else if (passGuessCount === 1) {
+    changePlayers();
+    passGuessCount = 0;
+    enableOtherQuestions();
+    questionDisplay1.textContent = " ";
+    disablePassGuess();
+    enableNextIfAllQuestionsClicked();
   }
 });
 
 let player1ScoreToPass;
 let player2ScoreToPass;
+let currentPlayerToPass;
 
-next.addEventListener("click", ()=>{
-goToNextRound();
-alert("you go to the next round");
-})
+player1ScoreToPass = player1Score;
+player2ScoreToPass = player2Score;
+currentPlayerToPass = currentPlayer;
 
+function enableNextIfAllQuestionsClicked() {
+  const totalQuestions = 2;
+  if (
+    clickedQuestions === totalQuestions ||
+    player1ScoreToPass === 30000 ||
+    player2ScoreToPass === 30000
+  ) {
+    enableNextRound();
+  }
+}
 
-guess.addEventListener("click", ()=>{
-  passGuessCount++;
-  let userInput1=document.querySelector(".userAnswer1").value
-  if (userInput1.toLowerCase()===currentAnswer.toLowerCase()) {
+next.addEventListener("click", () => {
+  // Passing the scores and the turn:
+  const url = "http://127.0.0.1:5500/final-jeopardy.html?";
+  const searchParams = new URLSearchParams();
+  searchParams.append("player1Score", player1Score);
+  searchParams.append("player2Score", player2Score);
+  searchParams.append("currentPlayer", currentPlayer);
+  const queryString = searchParams.toString();
+  const URL = url + queryString;
+
+  window.location.href = URL;
+
+  player1ScoreToPass = player1Score;
+  player2ScoreToPass = player2Score;
+  currentPlayerToPass = currentPlayer;
+
+  alert("you go to the next round");
+});
+
+guess.addEventListener("click", () => {
+  let userInput1 = document.querySelector(".userAnswer1").value;
+  if (userInput1.toLowerCase() === currentAnswer.toLowerCase()) {
     addPoints(); // if right, add points
     enableOtherQuestions(); //enable other questions to choose from
     updateScoreDisplay(); // display score
-    console.log(`Player1:${player1Score}, Player2:${player2Score}`); 
-    enableNextRound();
+    console.log(`Player1:${player1Score}, Player2:${player2Score}`);
     disablePassGuess();
-  } else if (userInput1.toLowerCase()!=currentAnswer.toLowerCase() && passGuessCount ===2) {
+    enableNextIfAllQuestionsClicked();
+    questionDisplay1.textContent = " ";
+  } else if (
+    userInput1.toLowerCase() != currentAnswer.toLowerCase() &&
+    passGuessCount === 1
+  ) {
     takeAwayPoints(); // if wrong, take away points
+    passGuessCount++;
+    //disableOtherQuestions();
     updateScoreDisplay(); // display score
     console.log(`Player1:${player1Score}, Player2:${player2Score}`);
     changePlayers(); // change players
+    enableNextIfAllQuestionsClicked();
+
     enableOtherQuestions();
-    questionDisplay1.textContent=" ";
+    questionDisplay1.textContent = " ";
     disablePassGuess();
     passGuessCount = 0;
+    enableNextIfAllQuestionsClicked();
+  } else if (
+    userInput1.toLowerCase() != currentAnswer.toLowerCase() &&
+    passGuessCount === 0
+  ) {
+    takeAwayPoints(); // if wrong, take away points
+    passGuessCount++;
+    disableOtherQuestions();
+    updateScoreDisplay(); // display score
+    console.log(`Player1:${player1Score}, Player2:${player2Score}`);
+    changePlayers(); // change players
   }
-  
-  else if (userInput1.toLowerCase()!=currentAnswer.toLowerCase() && passGuessCount === 1) {
-    takeAwayPoints(); // if wrong, take away points
-    disableOtherQuestions();
-    updateScoreDisplay(); // display score
-    console.log(`Player1:${player1Score}, Player2:${player2Score}`);
-    changePlayers(); // change players
-  } 
-
-  else if (userInput1.toLowerCase()!=currentAnswer.toLowerCase() && passGuessCount === 0) {
-    takeAwayPoints(); // if wrong, take away points
-    disableOtherQuestions();
-    updateScoreDisplay(); // display score
-    console.log(`Player1:${player1Score}, Player2:${player2Score}`);
-    changePlayers(); // change players
-  } 
-})
+});
 
 window.addEventListener("load", () => {
-      if (window.location.pathname === "/round-2.html") {
-    document.querySelector("#turnRound").textContent = `Player ${currentPlayer}'s turn!`; // turns
+  if (window.location.pathname === "/round-2.html") {
+    document.querySelector(
+      "#turnRound"
+    ).textContent = `Player ${currentPlayer}'s turn!`; // turns
     updateScoreDisplay(); //updating the score after the turn
-  } 
+    disableNextRound();
+  }
 });
